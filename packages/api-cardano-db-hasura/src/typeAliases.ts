b38af10804f5c62f145d478c33e7186c3b6f104a
lgobbi-atix <lgobbi@atixlabs.com>
2021-05-31 16:55:30 -0300

    feat: implemente complexity in resolvers
    ref #EX-1644

--- packages/api-cardano-db-hasura/src/typeAliases.ts
+++ packages/api-cardano-db-hasura/src/typeAliases.ts
@@ -1,19 +1,3 @@
-import { ComplexityEstimator } from 'graphql-query-complexity'
 import { Asset } from './graphql_types'
 
 export type AssetWithoutTokens = Omit<Asset, 'tokenMints' | 'tokenMints_aggregate'>
-
-type ComplexityExtension = {
-  extensions: {
-    complexity: number | ComplexityEstimator;
-  };
-};
-
-type ComplexityMapping = { [key: string]: ComplexityExtension };
-
-export type FieldsComplexityMapping = {
-  [key: string]:
-    | FieldsComplexityMapping
-    | ComplexityMapping
-    | ComplexityExtension;
-};
